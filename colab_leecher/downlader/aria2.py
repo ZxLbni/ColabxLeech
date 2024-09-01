c4c9091a57e59118a499318ac00068e83004c59e
Pasindu Dilsan <139089887+pasindudilsan@users.noreply.github.com>
2024-09-01 11:48:49 +0530

    Update aria2.py

--- colab_leecher/downlader/aria2.py
+++ colab_leecher/downlader/aria2.py
@@ -105,7 +105,6 @@ async def on_output(output: str):
 
     if elapsed_time_seconds >= 120 and not Aria2c.link_info:
         logging.error("Failed to get download information ! Probably dead link 💀")
-    return 
     # Only Do this if got Information
     if total_size != "0B":
         # Calculate download speed
